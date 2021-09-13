39c248b72d1ab52c4dd5eb5143a6789effbbfabb
Harmin Jeong <harminjeong@gmail.com>
2021-09-13 19:52:25 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Harmin Jeong
+ */
+
+package org.example;
+import java.util.Scanner;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the input string? ");
+        String name = input.nextLine();
+        System.out.println(name+" has "+name.length()+" characters.");
+    }
+}
